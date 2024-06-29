0b836328bda3853a963cc1b9d3bbe06664336514
J.S.LI <jsli@linux.com>
2024-06-29 22:59:37 +0800

    fix(bulma): revert to bulma 0.9.4 to improve bundle size.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: "https://pedestrianlove.github.io/viteuwu/",
+    base: "https://pedestrianlove.github.io/testuwu/",
 });
